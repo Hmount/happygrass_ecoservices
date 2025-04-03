46995014bddfc08f1fa3e5189c626ad9f0dfa63d
Hmount <haileymount11@gmail.com>
2025-04-03 14:54:24 -0600

    Started wrangling soil moisture data across years into one dataframe. Need to get 2024 growing season from Jules, but for 2021-2023: wrangled, made nice plot, inital model built (sig. differences between years and months, but less with precip treatments), and calculated precent reductions.

---
+++ code/soil_moisture/soil_moisture_anlayses.R
@@ -0,0 +1,57 @@
+#### Soil moisture analyses
+#### test for significant difference in soil moisture between 
+#### precipittaon treatments and seeding treatments
+
+#### 4/3/25: some differences between soil moisture depending 
+#### on month and year, but less on drought trt (some interactions)
+#### Need to add 2024 growing season and I assume it will strengthen
+#### this relationship.
+
+library(tidyverse)
+
+## cleaned soil moisture data (all values in m3/m3 VWC)
+smdat <- read.csv("code/soil_moisture/clean_soilmoisture.csv")
+
+## ensure correct data format and variable names
+colnames(smdat) <- c("Treatment", "Date","Moisture") #rename trt to treatment
+smdat$Treatment <- as.factor(smdat$Treatment) #make factor
+smdat <- smdat %>% #seperate date column
+  separate(Date, into = c("Year", "Month", "Day"), sep = "-", remove = T)
+smdat$Year <- as.factor(smdat$Year) #make factor
+smdat$Month <- as.factor(smdat$Month) #make factor
+smdat$Day <- as.factor(smdat$Day) #make factor
+## filter for only growing season (May through August)
+## probes are unreliable in winter
+smdat <- smdat %>%
+  filter(Month %in% c("05", "06", "07", "08")) %>%
+  mutate(Month = droplevels(Month))  # Removes unused levels
+
+## find monthly average (for viewing and maybe plotting) (not sig. diff in models)
+smdat.month <- smdat %>%
+  group_by(Year, Month, Treatment) %>%
+  summarise(moisturemean = mean(Moisture))
+#find percent reduced per month per year
+reduction.month <- smdat.month %>%
+  pivot_wider(names_from = Treatment, values_from = moisturemean) %>%  # Spread cntl & drt into separate columns
+  mutate(percent_reduction = ((cntl - drt) / cntl) * 100)  # Calculate percent reduction
+
+## find annual average to use for analyses
+smdat.yr <- smdat %>%
+  group_by(Year, Treatment) %>%
+  summarise(moistureyr = mean(Moisture))
+#find percent reduced per month
+reduction.yr <- smdat.yr %>%
+  pivot_wider(names_from = Treatment, values_from = moistureyr) %>%  # Spread cntl & drt into separate columns
+  mutate(percent_reduction = ((cntl - drt) / cntl) * 100)  # Calculate percent reduction
+
+## mean reduction over course of experiment(currently only looks at
+## 2021-2023).
+sum(reduction.yr$percent_reduction)/3 # = 9.3% mean reduction
+
+
+### Test and model differences in soil moisture 
+summary(lm(Moisture~Year*Month*Treatment, smdat))
+ggplot(smdat, aes(y=Moisture, x=Month, fill=Treatment))+
+  geom_boxplot()+
+  scale_fill_manual(values=c("skyblue","tomato2"))+
+  facet_wrap(~Year)
---
+++ code/soil_moisture/soil_moisture_wrangling.R
@@ -0,0 +1,86 @@
+#### Soil moisture data wrangling and initial summary plot
+#### (code from happygrass-happyforb scripts)
+#### TO DO: 
+## 2024 growing season is incomplete! requested from Jules
+## Add 2024
+## make figure
+## calculate total percent reduction
+
+
+library(tidyverse)
+library(ggpubr)
+
+## WY (all values in m3/m3 VWC)
+sm.wy21 <- read.csv("data/soil_moisture_wy/Restoration_soilmoisture_2021.csv")
+sm.wy21 <- sm.wy21 %>% mutate(across(-1, as.numeric))
+sm.wy22 <- read.csv("data/soil_moisture_wy/Restoration_soilmoisture_2022.csv")
+sm.wy22 <- sm.wy22 %>% mutate(across(-1, as.numeric))
+sm.wy23 <- read.csv("data/soil_moisture_wy/Restoration_soilmoisture_2023(toAug).csv")
+sm.wy23 <- sm.wy23 %>% mutate(across(-1, as.numeric))
+sm.wy24 <- read.csv("data/soil_moisture_wy/Restoration_soilmoisture_2024.csv")
+sm.wy24 <- sm.wy24 %>% mutate(across(-1, as.numeric))
+sm.wy <- bind_rows(sm.wy21,sm.wy22,sm.wy23, sm.wy24) #combine all annual soil moisture data
+sm.wy <- sm.wy %>% mutate(Timestamp = mdy_hm(Measurement.Time))
+
+#pivot longer and combine timestamps into daily soil moisture value
+sm.wy.long <- sm.wy %>% pivot_longer(where(is.numeric), names_to = "probenumber", values_to = "Moisture")
+sm.wy.long$Block <- gsub("X(\\d+)_Port\\d+", "\\1", sm.wy.long$probenumber)  # Extract block number
+sm.wy.long$Port <- gsub(".*_Port([0-9]+)", "\\1", sm.wy.long$probenumber)   # Extract port number
+# drought blocks
+droughtblocks <- data.frame(Block= c(20,22,27,30,33,42,49,50,51,58),
+                            trt = c("drt","drt","cntl","drt","cntl","cntl","cntl","drt","cntl","drt"))
+sm.wy.long <- merge(sm.wy.long, droughtblocks)
+
+sm.wy.long <- (sm.wy.long %>% mutate(Date = floor_date(Timestamp, unit = "day"))
+               %>% select(Date, trt, Moisture,Block)
+               %>% group_by(trt, Date)
+               %>% summarize(Moisture = mean(Moisture, na.rm = TRUE))
+               %>% arrange(trt, Date))
+
+write.csv(sm.wy.long, "code/soil_moisture/clean_soilmoisture.csv", row.names = F)
+
+### create pretty soil moisture figure 
+smwy <- sm.wy.long %>% select(trt,Date,Moisture) #keep only relevant variables
+smwy$Treatment <- as.factor(smwy$trt) #make factor
+
+## Create pretty plot with correct colors, axes, and shade growing season
+## separate out growing seasons in their own dataframes and re-combine
+#Create a data.frame for the shaded growing season WY (May - August)
+wyshade_data <- data.frame(
+  xmin = as.POSIXct(c("2021-05-01", "2022-05-01", "2023-05-01", "2024-05-01"), tz = "UTC"),
+  xmax = as.POSIXct(c("2021-09-01", "2022-09-01", "2023-09-01", "2024-09-01"), tz = "UTC"),
+  ymin = -Inf,  # Extend shading to the bottom of the plot
+  ymax = Inf    # Extend shading to the top of the plot
+)
+#create sequence to use for x axis breaks on both plots
+x_breaks <- seq(as.POSIXct("2021-01-01"), as.POSIXct("2024-09-01"), by = "6 months")  # Example: every 6 months
+
+# make WY plot
+wysmplot <- ggplot(smwy, (aes(y=Moisture, x=Date, col=trt)))+
+  geom_line()+
+  # ylab(expression(
+  #   Soil ~ volumetric ~ water ~ content ~ (m ^ 3 ~ m ^ -3)
+  # ))+
+  scale_x_datetime(
+    limits = as.POSIXct(c("2021-01-01 00:00:00", "2024-09-01 00:00:00"), tz = "UTC"),
+    date_labels = "%Y-%b",  # Optional: Custom labels like "2021-Jan"
+    breaks = x_breaks
+  )+
+  scale_y_continuous(breaks=c(.10,.15,.20,.25,.30,.35))+
+  labs(col="Precipitation 
+Treatment", x=" ", y=" ")+
+  scale_color_manual(values = c("blue","skyblue"), labels=c("Ambient", "Reduction"))+
+  geom_rect(
+    data = wyshade_data, 
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
+    inherit.aes = F,  # Prevents inheriting aesthetics from the main plot
+    fill = "grey18",        # Choose a shading color
+    alpha = 0.2           # Set transparency
+  ) +
+  theme_minimal()+
+  theme(legend.position = "right",
+        legend.key.size = unit(0.5, "cm"),    # Reduce the size of legend keys
+        legend.text = element_text(size = 8), # Make legend text smaller
+        legend.title = element_text(size = 9),# Adjust title size (optional)
+        legend.spacing.y = unit(0.2, "cm"),
+        axis.ticks.x = element_line(size=.5))
